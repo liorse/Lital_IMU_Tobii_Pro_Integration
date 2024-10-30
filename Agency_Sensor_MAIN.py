63cbc4393311e5ed523ecb35878b1dab6f92fa9b
liors <lior.segev@weizmann.ac.il>
2024-10-30 16:53:17 +0200

    Connected to all four meta's and started streaming fusion data

---
+++ Agency_Sensor_MAIN.py
@@ -0,0 +1,42 @@
+from ScopeFoundry import BaseMicroscopeApp
+from HW_MetaMotionRL import MetaMotionRLHW
+
+class FancyMicroscopeApp(BaseMicroscopeApp):
+
+    # this is the name of the microscope that ScopeFoundry uses 
+    # when storing data
+    name = 'Agency Sensor'
+    
+    # You must define a setup function that adds all the 
+    # capablities of the microscope and sets default settings
+    def setup(self):
+        
+        #Add App wide settings
+        
+        #Add hardware components
+        print("Adding Hardware Components")
+        self.add_hardware(MetaMotionRLHW(self, name='LeftHandMeta', MAC="F3:F1:E2:D3:6E:A7"))
+        self.add_hardware(MetaMotionRLHW(self, name='RightHandMeta', MAC="C2:26:C4:65:45:54"))
+        self.add_hardware(MetaMotionRLHW(self, name='LeftLegMeta', MAC="E1:39:04:67:C2:9B"))
+        self.add_hardware(MetaMotionRLHW(self, name='RightLegMeta', MAC="C7:B2:76:3D:6B:1D"))
+        
+        # self.add_hardware(MetaMotionRLHW(self, name='rightHandMeta'), MAC="C2:26:C4:65:45:54")
+        
+
+        #Add measurement components
+        print("Create Measurement objects")
+
+        # Connect to custom gui
+        
+        # load side panel UI
+        
+        # show ui
+        self.ui.show()
+        self.ui.activateWindow()
+
+
+if __name__ == '__main__':
+    import sys
+    
+    app = FancyMicroscopeApp(sys.argv)
+    sys.exit(app.exec_())
---
+++ HW_MetaMotionRL.py
@@ -0,0 +1,88 @@
+from ScopeFoundry import HardwareComponent
+# import our low level device object class (previous section)
+from mbientlab.metawear import MetaWear, libmetawear, parse_value
+from mbientlab.metawear.cbindings import *
+from mbientlab.warble import * 
+import platform
+
+
+class MetaMotionRLHW(HardwareComponent):
+    
+    ## Define name of this hardware plug-in
+    name = 'MetaMotionRL'
+
+    def __init__(self, app, name=None, debug=False, MAC="F3:F1:E2:D3:6E:A7"):
+        self.debug = debug
+        self.MAC = MAC
+        HardwareComponent.__init__(self, app, name=name)
+
+    def setup(self):
+        # I would need access to the following parameters
+        # 1. data rate in HZ for the fusion algorithm
+        # 2. choose linear acceleration in fusion algorithm - this should be chosen be default and not open to the user
+        # 3. choose the range of the accelerometer in the fusion algorithm
+        self.settings.New(name='MAC', initial=self.MAC, dtype=str, ro=True)
+        self.settings.New(name='start_streaming', initial=False, dtype=bool, ro=False)
+        
+        
+    def data_handler(self, ctx, data):
+        #print("Linear Acceleration: ({0}, {1}, {2})".format(data.x, data.y, data.z))
+        print(parse_value(data))
+
+    def start_data_fusion_stream(self, start):
+        if start:
+            print("start Streaming")
+            libmetawear.mbl_mw_sensor_fusion_enable_data(self.device.board, SensorFusionData.LINEAR_ACC)
+            libmetawear.mbl_mw_sensor_fusion_start(self.device.board)
+        else:
+            print("stop Streaming")
+            libmetawear.mbl_mw_sensor_fusion_stop(self.device.board)
+
+    def connect(self):
+        # Open connection to the device:
+        self.device = MetaWear(self.settings['MAC'])
+        self.device.connect()
+
+        print("Connected to " + self.device.address + " over " + ("USB" if self.device.usb.is_connected else "BLE"))
+        print("Device information: " + str(self.device.info))
+
+        # Set the linear acceleration in the fusion algorithm
+        #self.set_linear_acc_in_fusion()
+        # Set the fusion algorithm to linear acceleration
+        #self.set_fusion_to_LinAcc()
+        
+        # setup the device for streaming data
+        self.callback = FnVoid_VoidP_DataP(self.data_handler)
+
+        print("Configuring fusion on meta device")
+
+        libmetawear.mbl_mw_sensor_fusion_set_mode(self.device.board, SensorFusionMode.NDOF);
+        libmetawear.mbl_mw_sensor_fusion_set_acc_range(self.device.board, SensorFusionAccRange._8G)
+        libmetawear.mbl_mw_sensor_fusion_set_gyro_range(self.device.board, SensorFusionGyroRange._2000DPS)
+        libmetawear.mbl_mw_sensor_fusion_write_config(self.device.board)
+
+        self.signal = libmetawear.mbl_mw_sensor_fusion_get_data_signal(self.device.board, SensorFusionData.LINEAR_ACC)
+        libmetawear.mbl_mw_datasignal_subscribe(self.signal, None, self.callback)
+        
+        self.settings.start_streaming.connect_to_hardware(write_func=self.start_data_fusion_stream)
+   
+    
+    def disconnect(self):
+
+        # disconnect from hardware
+        self.settings.disconnect_all_from_hardware()
+        try:
+            libmetawear.mbl_mw_datasignal_unsubscribe(self.signal)
+            self.device.disconnect()
+            # unsubscribe from data signal
+            
+
+        except AttributeError:
+            print("called before device was connected")
+            
+        # remove all hardware connections to settings
+        self.settings.disconnect_all_from_hardware()
+        
+        # Don't just stare at it, clean up your objects when you're done!
+        #if hasattr(self, 'randgen_dev'):
+        #    del self.randgen_dev
